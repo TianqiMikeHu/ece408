ff248da8df0a1b70fd77edcd214a4e5cfdc405ff
Tianqi Hu <tianqimikehu2000@gmail.com>
2020-11-17 04:07:56 -0600

    constant memory for weights

--- new-forward.cu
+++ new-forward.cu
@@ -1,7 +1,7 @@
 #include <cmath>
 #include <iostream>
 #include "gpu-new-forward.h"
-
+__constant__ float mask[16*4*7*7];
 __global__ void conv_forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
     /*
@@ -32,6 +32,7 @@ __global__ void conv_forward_kernel(float *y, const float *x, const float *k, co
 #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
 #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
 #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+#define mask4d(i3, i2, i1, i0) mask[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
     // Insert your GPU convolution kernel code here
     int w = blockIdx.x*16+threadIdx.x;
@@ -43,14 +44,14 @@ __global__ void conv_forward_kernel(float *y, const float *x, const float *k, co
             for(int c=0; c<C; c++){
                 for(int p=0; p<K; p++){
                     for(int q=0; q<K; q++){
-                        sum += x4d(b,c,h+p,w+q)*k4d(blockIdx.z,c,p,q);
+                        sum += x4d(b,c,h+p,w+q)*mask4d(blockIdx.z,c,p,q);
                     }
                 }
             }
             y4d(b,blockIdx.z,h,w) = sum;
         }
     }
-   
+
 
 #undef y4d
 #undef x4d
@@ -62,15 +63,16 @@ __host__ void GPUInterface::conv_forward_gpu(float *host_y, const float *host_x,
     // Declare relevant device pointers
     float* device_y;
     float* device_x;
-    float* device_k;
+    float* device_k = NULL;
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
     // Allocate memory and copy over the relevant data structures to the GPU
     cudaMalloc((void**)&device_y, B*M*H_out*W_out*sizeof(float));
     cudaMalloc((void**)&device_x, B*C*H*W*sizeof(float));
-    cudaMalloc((void**)&device_k, M*C*K*K*sizeof(float));
+    //cudaMalloc((void**)&device_k, M*C*K*K*sizeof(float));
     cudaMemcpy(device_x, host_x, B*C*H*W*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(device_k, host_k, M*C*K*K*sizeof(float), cudaMemcpyHostToDevice);
+    //cudaMemcpy(device_k, host_k, M*C*K*K*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpyToSymbol(mask, host_k, M*C*K*K*sizeof(float));
     // Set the kernel dimensions and call the kernel
 	dim3 gridDim(ceil(1.0*W_out/16), ceil(1.0*H_out/16), M);
 	dim3 blockDim(16,16,1);
@@ -78,7 +80,7 @@ __host__ void GPUInterface::conv_forward_gpu(float *host_y, const float *host_x,
     // Copy the output back to host
     cudaMemcpy(host_y, device_y, B*M*H_out*W_out*sizeof(float), cudaMemcpyDeviceToHost);
     // Free device memory
-    cudaFree(device_k);
+    //cudaFree(device_k);
     cudaFree(device_x);
     cudaFree(device_y);
     // Useful snippet for error checking
